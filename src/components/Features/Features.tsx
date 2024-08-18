cf4ea66df11497e84f6da949aae59ed977e0d710
Krishnendu Das <86651039+itskdhere@users.noreply.github.com>
2024-08-18 12:08:13 +0530

    add Features section component

---
+++ src/components/Features/Features.tsx
@@ -0,0 +1,84 @@
+import {
+  Badge,
+  Group,
+  Title,
+  Text,
+  Card,
+  SimpleGrid,
+  Container,
+  rem,
+  useMantineTheme,
+} from "@mantine/core";
+import { IconGauge, IconUser, IconCookie } from "@tabler/icons-react";
+import classes from "./Features.module.css";
+
+const mockdata = [
+  {
+    title: "Extreme performance",
+    description:
+      "This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit",
+    icon: IconGauge,
+  },
+  {
+    title: "Privacy focused",
+    description:
+      "People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma",
+    icon: IconUser,
+  },
+  {
+    title: "No third parties",
+    description:
+      "They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves",
+    icon: IconCookie,
+  },
+];
+
+const Features = () => {
+  const theme = useMantineTheme();
+  const features = mockdata.map((feature) => (
+    <Card
+      key={feature.title}
+      shadow="md"
+      radius="md"
+      className={classes.card}
+      padding="xl"
+    >
+      <feature.icon
+        style={{ width: rem(50), height: rem(50) }}
+        stroke={2}
+        color={theme.colors.blue[6]}
+      />
+      <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+        {feature.title}
+      </Text>
+      <Text fz="sm" c="dimmed" mt="sm">
+        {feature.description}
+      </Text>
+    </Card>
+  ));
+
+  return (
+    <Container id="features" size="lg" className={classes.wrapper}>
+      <Group justify="center">
+        <Badge variant="filled" size="lg">
+          Features
+        </Badge>
+      </Group>
+
+      <Title order={2} className={classes.title} ta="center" mt="sm">
+        Features that you will love
+      </Title>
+
+      <Text c="dimmed" className={classes.description} ta="center" mt="md">
+        We are focused on providing the best experience for our users. Here are
+        some of the features that you will love.
+      </Text>
+
+      <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl" mt={50}>
+        {features}
+      </SimpleGrid>
+    </Container>
+  );
+};
+
+export default Features;
